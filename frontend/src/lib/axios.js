87e297e73312708645657271ce2c33e222452670
Aabhash Anand <abhasanand07@gmail.com>
2026-05-29 11:35:06 +0530

    last

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-alert("API URL = " + import.meta.env.VITE_API_URL);
-
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
